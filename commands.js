cb3944ce2a0021629239fbe238570237077c775a
Glitch (glitch-hello-node) <none>
2022-06-30 02:03:59 +0000

    👳🏖 Checkpoint ./commands.js:44296187/4255 ./app.js:44296187/3538

--- commands.js
+++ commands.js
@@ -1,131 +1,32 @@
 import 'dotenv/config';
-import { Client, Intents, MessageActionRow, MessageButton } from 'discord.js';
-import { getRandomEmoji, DiscordRequest } from './utils.js';
 
+import { DiscordRequest } from './utils.js';
 
-//Gateway client
-const client = new Client({intents: [Intents.FLAGS.GUILDS]});
-
-
-//custom IDs for interactions below (can also just pass strings)
-const Interactions = {
-  START_BUTTON: 'start',
-  DISMISS_BUTTON: 'dismiss'
+//Command payloads 
+const TEST_COMMAND = {
+  name: 'test',
+  description: 'oh man hope this works',
+  type: 1,
 };
 
-//map games, including character name, channel IDs, etc.
-let gameStates = {};
+//Options
+const GAME_COMMAND = {
+  name: 'game',
+  description: 'Start game',
+  options: [
+    {
+      type: 3,
+      name: 'character',
+      value: 'character',
+      description: 'enter the name of your character',
+      required: 'true',
+    },
+  ],
+  type: 1,
+};
 
-client.once('ready', () => {
-  //Gateway connection is active
-  console.log('ready');
-});
 
-
-//Handles application commands
-client.on('interactionCreate', async (interaction) => {
-  console.log('Interaction received: ', interaction);
-  //Return if it's not a command
-  if (!interaction.isCommand()) return;
-  const { commandName, user } = interaction;
+async function istallCommands(...commands) {
+  const installCommandEndpoint = '/applications/${process.env.APP_ID}/commands';
   
-  if (commandName == 'game') {
-    const characterName = interaction.options.getString('character');
-    //Saves their choice of character name
-    gameStates[user.id] = {
-      name: characterName,
-    };
-    
-    const row = new MessageActionRow().addComponents (
-      new MessageButton()
-        .setCustomId(Interactions.START_BUTTON)
-        .setLabel('Start Game')
-        .setStyle('PRIMARY'),
-      new MessageButton()
-        .setCustomID(Interactions.DISMISS_BUTTON)
-        .setLabel('Dismiss')
-        .setStyle('SECONDARY')
-    );
-
-    //just for fun, in utils.js
-    const randomEmoji = getRandomEmoji();
-    const messageText = "Let's start playing! " + randomEmoji;
-    
-    
-    //responding to interactions
-    await interaction.reply({ content: messageText, components: [row]});
-    
-  }
-});
-
-
-
-//handles buttons
-client.on('interactionCreate', async(interaction) => {
-  //return if it's not a button
-  if(!interaction.isButton()) return;
-  
-  const{ customId, user } = interaction;
-  
-  if (customId = Interactions.START_BUTTON) {
-    const characterName = gameStates[user.id].name;
-    
-    //creates channel
-    const createChannelEndpoint = '/guilds/${process.env.GUILD_ID}/channels';
-    
-    //handle errors from the DiscordAPI, *likely build this in
-    try {
-      //the method is in the REST API verb
-      let guildResponse = await DiscordRequest(createChannelEndpoint, {
-        method: 'POST',
-        body: {
-          name: 'game-${characterName}',
-          type: 0,
-          topic: 'Test channel for ${characterName}',
-      },
-    });
-    guildResponse = await guildResponse.json();
-    console.log('Channel Created: ', guildResponse);
-      
-    const gameChannelID = guildResponse.id;
-    gameStates[user.id].channel = gameChannelID;
-      
-    //empty components array removes buttons
-    //message formatting doc
-    await interaction.update({
-      content: 'Game tstarting in <#${gameChannelID}>',
-      components: [],
-    });
-      
-      //create message in channel
-      const createMessageEndpoint = '/channels/${gameChannelID}/messages';
-      
-      //posts in new channel
-  let msgResponse = await DiscordRequest(createMessageEndpoint, {
-      method: 'POST',
-      body: {
-      content: 'just startin a game in here',
-      },                  
-  });
-      
-  msgResponse = await msgResponse.json();
-    
-  console.log('Posted message: ', msgResponse);
-  } catch (e) {
-    console.log('Error starting game: ', e);
-  }
-       
-} else if (customId == Interactions.DISMISS_BUTTON) {
-  await interaction.update({ content: 'No game, ok ok', components: [] });
 }
-
-});
-
-//initiates connection to gateway
-client.login(process.env.BOT_TOKEN);
-    
-      
-
-
-          
-
